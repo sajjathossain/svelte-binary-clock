42ae25b4622677b72958407913a2ddf03ff157de
sajjathossain <sajjathossain.official@gmail.com>
2025-04-28 11:08:13 +0600

    feat: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    assetsInlineLimit: Infinity
+  },
   server: {
     open: true
   },
