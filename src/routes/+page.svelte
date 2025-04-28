9ccba844a5833a1f7ab4c94fbf7a61601d78691f
sajjathossain <sajjathossain.official@gmail.com>
2025-04-28 15:54:53 +0600

    style(+page): hide on smaller screen and add warning message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,9 @@
 {/snippet}
 
 <div class="flex h-full w-full flex-col items-center justify-center gap-12">
-	<div class="flex w-4/12 justify-between text-3xl font-bold">
+	<div
+		class="hidden w-8/12 justify-between text-3xl font-bold md:flex lg:w-5/12 lg:max-w-6/12 lg:min-w-4/12"
+	>
 		{#each [{ name: 'h', value: 0 }, { name: 'm', value: 2 }, { name: 's', value: 4 }] as ai, i (ai.name + ai.value + i)}
 			{@const first = binaryValues[ai.value]}
 			{@const second = binaryValues[ai.value + 1]}
@@ -56,6 +58,9 @@
 			</div>
 		{/each}
 	</div>
+	<p class="visible p-2 text-center text-2xl text-amber-400 md:hidden">
+		You have to use a larger screen to see the time.
+	</p>
 
 	<p class="text-4xl font-extrabold text-blue-500">Binary clock</p>
 </div>
