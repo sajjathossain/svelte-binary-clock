8da7ffac5b4f8ad04bdd2067d4b538653b11ea68
sajjathossain <sajjathossain.official@gmail.com>
2025-04-28 16:24:51 +0600

    chore(svelte.config): add more configs

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,18 @@ const config = {
 
   kit: {
     output: {
-      bundleStrategy: "inline"
+      bundleStrategy: "inline",
+      preloadStrategy: "preload-js"
+    },
+    prerender: {
+      concurrency: 1
     },
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: 'index.html',
+    }),
   }
 };
 
