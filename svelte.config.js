3e804589685ba937198cdf9ada0cc0ca74dcb8f2
sajjathossain <sajjathossain.official@gmail.com>
2025-04-28 11:11:36 +0600

    chore(svelte.config): update adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,8 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
+      pages: 'build',
+      assets: 'build',
       fallback: "index.html",
       precompress: true,
       strict: true
