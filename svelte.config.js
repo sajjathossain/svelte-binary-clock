6829de3a4268c6368c2b0c16f3d4081ecc606177
sajjathossain <sajjathossain.official@gmail.com>
2025-04-28 16:36:28 +0600

    chore(svelte.config): set prerender concurrency to 4

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       preloadStrategy: "preload-js"
     },
     prerender: {
-      concurrency: 1
+      concurrency: 4
     },
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
