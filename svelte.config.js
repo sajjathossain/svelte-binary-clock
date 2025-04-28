faf8e9cca84698c066fb71b365ddfcf74193884b
sajjathossain <sajjathossain.official@gmail.com>
2025-04-28 16:17:19 +0600

    chore(svelte.config): remove some unnecessary configs

--- svelte.config.js
+++ svelte.config.js
@@ -15,11 +15,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
       fallback: "index.html",
-      precompress: true,
-      strict: true
     }),
   }
 };
